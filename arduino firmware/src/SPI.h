b2bb98641d134230e041157865fc6f0bb9effd93
gorchy <gorchy@3b72a0b2-1e8c-11df-85a5-c94dbdc1783d>
2010-03-20 00:45:45 +0000

    ... last time I forgot to actually add the files for the SPI class.

---
+++ arduino firmware/src/SPI.h
@@ -0,0 +1,32 @@
+/*
+ * SPI.h
+ *
+ *  Created on: 2010-03-19
+ *      Author: Jeff
+ */
+
+#ifndef SPI_H_
+#define SPI_H_
+
+#include "../arduino/WProgram.h"
+#include "API.h"
+
+class SPI {
+public:
+	SPI(uint8_t, uint8_t, uint8_t);
+	SPI();
+	~SPI();
+
+	uint8_t  add_slave (uint8_t);	//returns index of "slave_pins" array, AKA Device ID
+	uint16_t send_get  (uint8_t, uint8_t, uint8_t);
+
+private:
+	uint8_t   CLK;
+	uint8_t   MOSI;
+	uint8_t   MISO;
+	uint8_t   slave_pins[8];
+};
+
+extern SPI MCP3308;
+
+#endif /* SPI_H_ */
